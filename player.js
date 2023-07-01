e9e0eb9b881261eeb239d77f588ac104c75c7cf6
onifernando1 <onifernando1@gmail.com>
2023-07-01 17:50:23 +0100

    player class

--- player.js
+++ player.js
@@ -1,7 +1,16 @@
 export class Player {
   constructor(game) {
     this.game = game;
-    this.x = x;
-    this.y = y;
+    this.image = playerDown;
+    this.width = width;
+    this.height = height;
+    this.x = 10;
+    this.y = 10;
+    this.frameX = 0;
+    this.frameY = 0;
+    this.maxFrame = maxFrame;
+    this.fps = 15;
+    this.frameInterval = 1000 / this.fps;
+    this.frameTimer = 0;
   }
 }

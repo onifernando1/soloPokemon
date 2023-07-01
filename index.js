5af50e025a67f83334a581f44476b493942ef429
onifernando1 <onifernando1@gmail.com>
2023-07-01 15:18:53 +0100

    get canvas, onload event, height and width

---
+++ index.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  const canvas1 = document.getElementById("canvas1");
+  canvas1.width = 1024;
+  canvas1.height = 576;
+  const c = canvas1.getContext("2d");
+});

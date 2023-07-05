37a4eeed417d057f1e6e2a3473045b3c2ac68e45
onifernando1 <onifernando1@gmail.com>
2023-07-05 01:01:53 +0100

    battle scene image and draw

--- battleAnimation.js
+++ battleAnimation.js
@@ -6,8 +6,7 @@ export class BattleAnimation {
 
   animate(timeStamp) {
     this.c.clearRect(0, 0, this.game.width, this.game.height);
-    this.c.fillStyle = "red";
-    this.c.fillRect(0, 0, 50, 50);
+    this.drawImage(battleScene, 0, 0, this.game.width, this.game.height);
     requestAnimationFrame(animate);
   }
 }
